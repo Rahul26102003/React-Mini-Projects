3f24afe230d7e601da91e60107054ca961141252
Rahul26102003 <2221032cse.coe@cgc.edu.in>
2024-04-01 16:42:30 +0530

    first commit

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Chai from "./Chai";
+function App() {
+  const username = "By Rahul Sharma"
+  return (
+    <>
+     <Chai/>
+     <h1>Chai aur react {username}</h1>
+    </> 
+  ); 
+}
+
+export default App;
+
